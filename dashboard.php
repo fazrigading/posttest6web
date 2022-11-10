82d3624e395a9805ae3dea28d5bbe04b7666f3fb
Fazri Gading <fazrigading@gmail.com>
2022-11-10 10:41:11 +0800

    posttest7 login register searching

--- dashboard.php
+++ dashboard.php
@@ -15,14 +15,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>User Dashboard</title>
+  <title>Dashboard</title>
   <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
 <body>
   <div>
+    <a href="index.php" role="button"> Back to Home</a>
+    <a href="catalog.php" role="button"> Catalog</a>
     <a href="logout.php" role="button"> Logout</a>
   </div>
-  <h1>User</h1>
+  <h1>Users</h1>
   <table border="1">
     <tr>
       <th>No</th>
